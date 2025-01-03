3ac003f7a4f3f415ed77465961325728ec78c560
Nikita Tsvetkov <tsv1@fastmail.com>
2025-01-03 20:30:46 +0400

    bump version → 0.1.0

--- vedro_unittest/__init__.py
+++ vedro_unittest/__init__.py
@@ -1,5 +1,5 @@
 from ._test_loader import UnitTestLoader
 from ._vedro_unittest import VedroUnitTest, VedroUnitTestPlugin
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __all__ = ("VedroUnitTest", "VedroUnitTestPlugin", "UnitTestLoader")
